d80fcd918c30e7bf3f322f616c0350b0d2a026f7
Artem Osadchyi <infernalrabbit@gmail.com>
2015-08-01 13:12:45 +0300

    Add Post entity

---
+++ src/main/java/org/insane/jblog/domain/Post.java
@@ -0,0 +1,92 @@
+package org.insane.jblog.domain;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Post {
+    private long id;
+    private String title;
+    private String content;
+    private Date creationDate;
+    private Date lastEditionDate;
+    private Set<String> tags;
+
+    @Id
+    @GeneratedValue
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @NotNull
+    @Size(max = 255)
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @NotNull
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @NotNull
+    @Past
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @NotNull
+    @Past
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "edited_at")
+    public Date getLastEditionDate() {
+        return lastEditionDate;
+    }
+
+    public void setLastEditionDate(Date lastEditionDate) {
+        this.lastEditionDate = lastEditionDate;
+    }
+
+    @ElementCollection
+    @CollectionTable(joinColumns = @JoinColumn(name = "post_id"))
+    @Column(name = "tag")
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
+
+}
